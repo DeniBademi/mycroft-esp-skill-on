90130e08bfe3d23ffc4e0944f81183bfee461101
DeniBademi <48317568+DeniBademi@users.noreply.github.com>
2019-03-16 19:12:32 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,22 +19,40 @@ class TodayHistorySkill(MycroftSkill):
 
         random_event_intent = IntentBuilder("RandomEventIntent").\
             require("RandomEventKeyword").build()
-        self.register_intent(random_event_intent, self.handle_random_event_intent)
+        self.register_intent(random_event_intent, self.handle_first_lamp_on_intent)
         
         random_event_intent2 = IntentBuilder("RandomEventIntent2").\
             require("secondLightKeyword").build()
-        self.register_intent(random_event_intent2, self.handle_second_lamp_intent)
+        self.register_intent(random_event_intent2, self.handle_second_lamp_on_intent)
+        
+        random_event_intent3 = IntentBuilder("RandomEventIntent3").\
+            require("RandomEventKeyword2").build()
+        self.register_intent(random_event_intent3, self.handle_first_lamp_off_intent)
+        
+        random_event_intent4 = IntentBuilder("RandomEventIntent4").\
+            require("secondLightKeyword2").build()
+        self.register_intent(random_event_intent4, self.handle_second_lamp_off_intent)
 
 
-    def handle_random_event_intent(self, message):
+    def handle_first_lamp_on_intent(self, message):
         url="https://10.106.0.225/lamp1/1"
         r = urllib.request.urlopen("https://10.106.0.225/lamp1/1", context=ssl.SSLContext()).read()
         self.speak("As you wish") 
     
-    def handle_second_lamp_intent(self, message):
+    def handle_second_lamp_on_intent(self, message):
         url="https://10.106.7.2/lamp2/1"
         r = urllib.request.urlopen("https://10.106.7.2/lamp2/1", context=ssl.SSLContext()).read()
         self.speak("As you wish") 
+        
+    def handle_first_lamp_off_intent(self, message):
+        url="https://10.106.0.225/lamp1/1"
+        r = urllib.request.urlopen("https://10.106.0.225/lamp1/0", context=ssl.SSLContext()).read()
+        self.speak("As you wish") 
+    
+    def handle_second_lamp_off_intent(self, message):
+        url="https://10.106.7.2/lamp2/1"
+        r = urllib.request.urlopen("https://10.106.7.2/lamp2/0", context=ssl.SSLContext()).read()
+        self.speak("As you wish")
 
     def stop(self):
         pass
